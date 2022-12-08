ed453c0d860ff35127aadd33bd5a1f6d37e6ef0a
Robert Herrick <rob_herrick@hotmail.com>
2022-12-08 11:57:36 -0500

    multiple loggers=> song list

--- src/wlogger.js
+++ src/wlogger.js
@@ -1,16 +1,44 @@
+//import winston from 'winston/lib/winston/config';
+const winston = require('winston')
+
 const { createLogger, format, transports } = require('winston')
 
+const winstonLogs = new winston.Container();
 
-const volume = format((info, opts) => {
+const addDate = format((info, opts) => {
     info.message = `${new Date().toISOString()}->${info.message}`
     return info;
   });
   
+  winstonLogs.add('wlogger',{
+      level: 'info',
+      format: format.combine(
+        addDate({}),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json()
+      ),
+      transports: [
+        //
+        // - Write to all logs with level `info` and below to `quick-start-combined.log`.
+        // - Write all logs error (and below) to `quick-start-error.log`.
+        //
+        new transports.File({ filename: './logs/error.log', level: 'error' }),
+        new transports.File({ filename: './logs/combined.log' }),
+        new transports.Console({
+          format: format.combine(
+            format.colorize(),
+            format.simple()
+          )
+        })
+      ]
+    
+  })
 
-const wlogger = createLogger({
+  winstonLogs.add('playedSongs',{
   level: 'info',
   format: format.combine(
-    volume({ yell: true }),
+    addDate({}),
     format.errors({ stack: true }),
     format.splat(),
     format.json()
@@ -20,8 +48,7 @@ const wlogger = createLogger({
     // - Write to all logs with level `info` and below to `quick-start-combined.log`.
     // - Write all logs error (and below) to `quick-start-error.log`.
     //
-    new transports.File({ filename: './logs/error.log', level: 'error' }),
-    new transports.File({ filename: './logs/combined.log' }),
+    new transports.File({ filename: './logs/playedSongs.log' }),
     new transports.Console({
       format: format.combine(
         format.colorize(),
@@ -33,4 +60,4 @@ const wlogger = createLogger({
 
 
 
-export default wlogger;
+export default winstonLogs;
